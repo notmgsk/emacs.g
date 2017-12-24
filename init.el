c5cbd4ba9a59954d2927b1598b2bcd637553c7ed
Mark Skilbeck <markskilbeck@gmail.com>
2017-12-24 20:13:48 +0000

    Assimilate which-key v3.0.2-18-gef384e7

--- init.el
+++ init.el
@@ -268,6 +268,13 @@
   (add-to-list 'tramp-default-proxies-alist
                (list (regexp-quote (system-name)) nil nil)))
 
+(use-package which-key
+  :defer 5
+  :diminish
+  :commands which-key-mode
+  :config
+  (which-key-mode))
+
 (progn ;     startup
   (message "Loading %s...done (%.3fs)" user-init-file
            (float-time (time-subtract (current-time)
