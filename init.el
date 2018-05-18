0114605c1483cd65e30d5e8ed781b85b5b7ddbbd
Mark Skilbeck <markskilbeck@gmail.com>
2018-05-18 09:32:09 +0100

    use offlineimap for mu4e update command
    probably better to use it this way without having offlineimap run as a daemon

--- init.el
+++ init.el
@@ -338,7 +338,7 @@
   (setq mu4e-maildir "~/Mail"
         mu4e-trash-folder "/Trash"
         mu4e-refile-folder "/Archive"
-        mu4e-get-mail-command "true"
+        mu4e-get-mail-command "offlineimap"
         mu4e-update-interval 300
         mu4e-compose-signature-auto-include nil
         mu4e-view-show-images t
