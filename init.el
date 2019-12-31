fce384e71783994a10c3fdccaf370258355d0f52
Mark Skilbeck <markskilbeck@gmail.com>
2019-12-31 09:27:06 -0800

    Prefer HTTPS to SSH (borg)

--- init.el
+++ init.el
@@ -19,7 +19,11 @@
 (progn                                  ;    `borg'
   (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
   (require  'borg)
-  (borg-initialize))
+  (borg-initialize)
+  ;; Prefer HTTPS URLs rather than SSH.
+  (setq borg-rewrite-urls-alist
+        '(("git@github.com:" . "https://github.com/")
+          ("git@gitlab.com:" . "https://gitlab.com/"))))
 
 (progn ;    `use-package'
   (setq use-package-enable-imenu-support t)
